ba8e51d7d23e1549e36e68c1b42f3f4f5f158180
Christopher Walker <Christopher.Walker@longbeach.gov>
2023-03-03 10:43:27 -0800

    Initial commit

---
+++ First Project.py
@@ -0,0 +1,12 @@
+test_list = ('apples', 'bananas', 'orange')
+
+for item in test_list:
+    print(f"I love {item}")
+
+n = 1
+while n != 10:
+    n += 1
+    print(f"{n}")
+
+if 'orange' in test_list:
+    print(f"time for some {test_list[2]}'s")
